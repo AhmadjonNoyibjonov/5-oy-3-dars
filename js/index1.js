dac1074ed40e4ca20e90a236daa9d255ff535438
AhmadjonNoyibjonov <ahmadjonnoyibjonov60@gmail.com>
2024-06-21 00:55:41 +0500

    project

---
+++ js/index1.js
@@ -0,0 +1,38 @@
+// 1-masala
+// function summ(n) {
+//     if(n == 0) {
+//         return 0
+//     }
+
+//     return n + summ(n-1)
+// }
+// console.log(summ(9));
+
+// 2-masala
+// const factarial = (n) => {
+//     if(n == 0){
+//         return 1
+//     }
+
+//     return n * factarial(n-1)
+// }
+// console.log(factarial(10));
+
+// 3-masala
+// let car = {
+//     name:"Spark",
+//     year:"2017",
+//     weight:"1500",
+//     color:"white",
+// }
+
+// let test = Object.entries(car).flat()
+
+// console.log(test);
+
+
+// 4-masala
+// let number = [2,4,6,7,9,12]
+
+// let result = JSON.stringify(number)
+// console.log(result);
